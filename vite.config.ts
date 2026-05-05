3d00002478d670f599ddb43c1e92151c579d10fe
sneha-m2000 <153909868+sneha-m2000@users.noreply.github.com>
2026-05-05 18:00:09 +0530

    Remove statsfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
     plugins: [
         react(),
-        visualizer({
-            open: true,
-        }),
     ],
 });
